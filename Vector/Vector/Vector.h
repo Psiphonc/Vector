b4e75b1cec816ea480ed5e2726bbb5b8bd02cad7
psiphonc <psiphonc@outlook.com>
2017-11-12 17:05:25 +0800

    Add default constructor

--- Vector/Vector/Vector.h
+++ Vector/Vector/Vector.h
@@ -1,9 +1,20 @@
 #pragma once
+#define DEFAULT_CAPACITY 10
+
+template<typename T>
 class Vector
 {
 public:
-	Vector();
+	Vector(T e=(T)0,int s=0,int c=DEFAULT_CAPACITY)
+	{
+		elem_ = new T[capacity_ = c];
+		for (int i = 0; i < s; elem_[i++] = e);
+	}
 	~Vector();
+protected:
+	T *elem_;
+	int capacity_;
+	int size_;
 
 };
 
